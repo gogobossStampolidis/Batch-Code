35b06c5bd3a4657c7f104cbcedbaf1906fb34933
gogobossStampolidis <82140482+gogobossStampolidis@users.noreply.github.com>
2021-04-25 14:45:18 +0300

    Create bar.bat

---
+++ Simple Progress Bar/bar.bat
@@ -0,0 +1,106 @@
+echo off
+cls
+
+chcp 65001>nul
+
+
+echo █▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████▓▒░░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████▓▒░░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████▓▒░░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████▓▒░░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████▓▒░░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████▓▒░░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████▓▒░░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████████▓▒░░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████████▓▒░░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████████▓▒░░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████████▓▒░░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████████████▓▒░░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████████████▓▒░░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████████████▓▒░░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████████████▓▒░░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████████████████▓▒░░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████████████████▓▒░░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████████████████▓▒░░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████████████████▓▒░░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████████████████████▓▒░░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████████████████████▓▒░░░░
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████████████████████▓▒░░░
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████████████████████▓▒░░
+timeout 1 /NOBREAK>nul
+cls
+echo ██████████████████████████████▓▒░
+timeout 1 /NOBREAK>nul
+cls
+echo ███████████████████████████████▓▒
+timeout 1 /NOBREAK>nul
+cls
+echo ████████████████████████████████▓
+timeout 1 /NOBREAK>nul
+cls
+echo █████████████████████████████████
+timeout 5 /NOBREAK>nul
+cls
+echo FINISHED!
